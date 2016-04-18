b5d693736e5b5a3d9ea136247df10987d1ded8e8
Julien HENRY <julien.henry@sonarsource.com>
2016-04-18 16:13:34 +0200

    Add JavaScript example

---
+++ CsvParser/src/main/js/sample.js
@@ -0,0 +1,26 @@
+function sum(a, b) {
+  return a + b;
+}
+
+function main() {
+  var total;
+  for (var i = 0; i < 10; i++) {
+    for (var j = 0; j < 10; i++) {
+      total += sum(i, j);
+    }
+  }
+  // TODO
+  var str = "Foo";
+  return str;
+}
+
+function divide(divisor, dividend) {
+  return divisor/dividend;
+}
+
+function doTheThing() {
+  var divisor = 15;
+  var dividend = 5;
+
+  var result = divide(dividend, divisor);
+}
